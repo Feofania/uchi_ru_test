e5da6b8a1c2c81c9b3b536c43bdab3820f5aa227
Feofania <evgeniy_will@list.ru>
2017-07-17 14:13:24 +0400

    Update application.js

--- application.js
+++ application.js
@@ -1,1 +1,52 @@
+$(document).ready(function() {
+  var randomFist = Math.round(Math.random() * 4) + 6;
+  var min = 11 - randomFist;
+  var max = 14 - randomFist;
+  var randomSecond = Math.round(Math.random() * ( max - min + 1)) + min;
+  
+  switch(randomFist) {
+    case '6': 
+      drawingSecondArrow(randomSecond);
+      break;
+    case '7': 
+      drawingSecondArrow(randomSecond);
+      break;
+    case '8': 
+      drawingSecondArrow(randomSecond);
+      break;
+    case '9': 
+      drawingSecondArrow(randomSecond);
+      break;
+  }
+  
+  var drawingFistArrow = function() {
+    
+  };
+  
+  var drawingSecondArrow = function(secondArrow) {
+      switch(secondArrow) {
+      case '2': 
 
+        break;
+      case '3': 
+
+        break;
+      case '4': 
+
+        break;
+      case '5': 
+
+        break;
+      case '6': 
+
+        break;
+      case '7': 
+
+        break;
+      case '8': 
+
+        break;
+    }
+  };
+  
+});
