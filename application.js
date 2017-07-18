2256987348bac6cc182d72b34a62f75a78c9cc4e
Feofania <evgeniy_will@list.ru>
2017-07-18 16:55:21 +0400

    Update application.js

--- application.js
+++ application.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
-  var randomFrist = Math.round(Math.random() * 4) + 6;
-  var min = 11 - randomFrist;
-  var max = 14 - randomFrist;
+  var randomFirst = Math.round(Math.random() * 4) + 6;
+  var min = 11 - randomFirst;
+  var max = 14 - randomFirst;
   var randomSecond = Math.round(Math.random() * ( max - min + 1)) + min;
+  
+  var quention = $("<p class='quention'>" + randomFirst + " + " + randomSecond + " = ? </p>");
+  $('.quention').append(quention);
+  
+  var sprite = $("<img class='sprite' src='sprites/sprite" + randomFirst + ".png'>");
+  $('.sprite').append(sprite);
+  
+  var arrowAlfa = $("<input class='arrowAlfa" + randomFirst + "' input type='text' size='5'>");
+  $('.arrowAlfa').append(arrowAlfa);
   
   
 });
