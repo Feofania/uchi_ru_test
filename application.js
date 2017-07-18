65b371e60e3585bc654a9e05c3c1ca0fbabfa01d
Feofania <evgeniy_will@list.ru>
2017-07-18 15:47:20 +0400

    Update application.js

--- application.js
+++ application.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-  var randomFist = Math.round(Math.random() * 4) + 6;
-  var min = 11 - randomFist;
-  var max = 14 - randomFist;
+  var randomFrist = Math.round(Math.random() * 4) + 6;
+  var min = 11 - randomFrist;
+  var max = 14 - randomFrist;
   var randomSecond = Math.round(Math.random() * ( max - min + 1)) + min;
   
-  switch(randomFist) {
+  switch(randomFrist) {
     case '6': 
       drawingSecondArrow(randomSecond);
       break;
