48b57f79acfdbffb588415ff74bf9260ac4f2f32
Evgenia Perevedentseva <evgeniy_will@list.ru>
2017-07-20 13:05:03 +0300

    5

--- application.js
+++ application.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
   $('.arrowAlfa'+randomFirst).on('keyup',  function() {
     var alfa = $(this).val();
     if (alfa == randomFirst) {
-      var alfaLab = $("<label for='arrowAlfa"+alfa+"' class='arrowAlfa"+randomFirst + "'>"+alfa+"</label>");
+      var alfaLab = $("<label class='arrowAlfa"+randomFirst+"'>"+alfa+"</label>");
       $('.arrowAlfa'+randomFirst).remove();
       $('.question').append(alfaLab);
 
@@ -24,36 +24,36 @@ $(document).ready(function() {
 
       var arrowBeta = $("<input class='arrowBeta"+randomFirst+randomSecond+"' type='text' size='1'>");
       $('.question').append(arrowBeta);
+      console.log('1');
     } else {
       $(this).toggleClass("notArrow");
       $(this).closest('.question').find('.randomFirst').toggleClass("notRandom");
     }
-  });
-  
-  $('.arrowBeta'+randomFirst+randomSecond).on('keyup',  function() {
-    var beta = $(this).val();
-    if (beta == randomSecond) {
-      var betaLab = $("<label for='arrowBeta" + beta + "'>" + beta + "</label>");
-      $('.question').append(betaLab);
 
-      var answer = $("<input class='answer' type='text' size='1'>");
-      $('.question').append(answer);
-      $('p').text(randomFirst+' + '+randomSecond+' = ');
-    } else {
-      $(this).toggleClass("notArrow");
-      $(this).closest('.question').find('.randomSecond').toggleClass("notRandom");
-    }
-  });
-  
-  $('.answer').on('keyup', function(){
-    var an = $(this).val();
-    if (an == randomFirst+randomSecond) {
-      var answerLab = $("<label class='answer'>"+an+"</label>");
-      $('.answer').remove();
-      $('.question').append(answerLab);
-      $('p').text(randomFirst+' + '+randomSecond+' = '+an);
-    } else {
-      $(this).toggleClass("notArrow");
-    }
+    $('.question').on('keyup', '.arrowBeta'+randomFirst+randomSecond,  function() {
+        var beta = $('.arrowBeta'+randomFirst+randomSecond).val();
+        if (beta == randomSecond) {
+          var betaLab = $("<label class='arrowBeta"+randomFirst+randomSecond+"'>"+beta+"</label>");
+          $('.arrowBeta'+randomFirst+randomSecond).remove();
+          $('.question').append(betaLab);
+
+          var answer = $("<input class='answer' type='text' size='1'>");
+          $('.question').append(answer);
+          $('p').text(randomFirst+' + '+randomSecond+' = ');
+        } else {
+          $(this).toggleClass("notArrow");
+          $(this).closest('.question').find('.randomSecond').toggleClass("notRandom");
+        }
+
+        $('.answer').on('keyup', function(){
+            var an = $(this).val();
+            if (an == randomFirst+randomSecond) {
+              $('.answer').remove();
+              $('p').text(randomFirst+' + '+randomSecond+' = '+an);
+            } else {
+              $(this).toggleClass("notArrow");
+            }
+          });
+      });
   });
 });
