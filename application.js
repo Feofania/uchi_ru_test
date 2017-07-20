746d41f673c0b5005ac0e4fd8f5ba357eda69622
Evgenia Perevedentseva <evgeniy_will@list.ru>
2017-07-20 11:58:10 +0300

    4

--- application.js
+++ application.js
@@ -2,42 +2,58 @@ $(document).ready(function() {
   var randomFirst = Math.round(Math.random() * 3) + 6;
   var min = 11 - randomFirst;
   var max = 14 - randomFirst;
-  var randomSecond = Math.round(Math.random() * ( max - min + 1)) + min;
+  var randomSecond = Math.round(Math.random() * ( max - min)) + min;
   
-  var expression = $("<p class='expression'> <span class='randomFirst'>" + randomFirst + "</span> + <span class='randomSecond'>" + randomSecond + "</span> = ? </p>");
+  var expression = $("<p class='expression'> <span class='randomFirst'>"+randomFirst+"</span> + <span class='randomSecond'>"+randomSecond+"</span> = ? </p>");
   $('.question').append(expression);
 
-  var sprite = $("<img class='sprite' src='sprites/sprite" + randomFirst + ".png'>");
+  var sprite = $("<img class='sprite' src='sprites/sprite"+randomFirst+".png'>");
   $('.question').append(sprite);
 
-  var arrowAlfa = $("<input class='arrowAlfa" + randomFirst + "' type='text' size='1'>");
+  var arrowAlfa = $("<input class='arrowAlfa"+randomFirst+"' type='text' size='1'>");
   $('.question').append(arrowAlfa);
 
   $('.arrowAlfa'+randomFirst).on('keyup',  function() {
     var alfa = $(this).val();
     if (alfa == randomFirst) {
-      var alfaLab = $("<label for='arrowAlfa" + alfa + "' class='arrowAlfa" + randomFirst + "'>" + alfa + "</label>");
+      var alfaLab = $("<label for='arrowAlfa"+alfa+"' class='arrowAlfa"+randomFirst + "'>"+alfa+"</label>");
       $('.arrowAlfa'+randomFirst).remove();
       $('.question').append(alfaLab);
-      //задать новую картинку
-      //задать новый inputBeta
+
+      $('.sprite').attr('src', 'sprites/sprite'+randomFirst+randomSecond+'.png');
+
+      var arrowBeta = $("<input class='arrowBeta"+randomFirst+randomSecond+"' type='text' size='1'>");
+      $('.question').append(arrowBeta);
     } else {
-      $(this).toggleClass("notArrowAlfa");
-      $(this).closest('.question').find('.randomFirst').toggleClass("notRandomFirst");
+      $(this).toggleClass("notArrow");
+      $(this).closest('.question').find('.randomFirst').toggleClass("notRandom");
     }
   });
   
-  $('.arrowBeta'+randomSecond).on('keyup',  function() {
+  $('.arrowBeta'+randomFirst+randomSecond).on('keyup',  function() {
     var beta = $(this).val();
     if (beta == randomSecond) {
       var betaLab = $("<label for='arrowBeta" + beta + "'>" + beta + "</label>");
       $('.question').append(betaLab);
-      //задать новый inputAnswer
+
+      var answer = $("<input class='answer' type='text' size='1'>");
+      $('.question').append(answer);
+      $('p').text(randomFirst+' + '+randomSecond+' = ');
     } else {
-      $(this).toggleClass("notArrowBeta");
-      $(this).closest('.question').find('.randomSecond').toggleClass("notRandomSecond");
+      $(this).toggleClass("notArrow");
+      $(this).closest('.question').find('.randomSecond').toggleClass("notRandom");
     }
   });
   
-  //слушать inputAnswer
+  $('.answer').on('keyup', function(){
+    var an = $(this).val();
+    if (an == randomFirst+randomSecond) {
+      var answerLab = $("<label class='answer'>"+an+"</label>");
+      $('.answer').remove();
+      $('.question').append(answerLab);
+      $('p').text(randomFirst+' + '+randomSecond+' = '+an);
+    } else {
+      $(this).toggleClass("notArrow");
+    }
+  });
 });
