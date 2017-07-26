bf265dcb1d84a80729718a1505d31de7838802c1
Evgenia Perevedentseva <evgeniy_will@list.ru>
2017-07-26 08:32:40 +0300

    8

--- application.js
+++ application.js
@@ -5,37 +5,40 @@ $(document).ready(function() {
     var randomSecond = Math.round(Math.random() * ( max - min)) + min;
 
     //пример
-    var expression = $("<p class='expression'> <span class='randomFirst'>"+randomFirst+"</span> + <span class='randomSecond'>"+randomSecond+"</span> = ? </p>");
+    var expression = $("<div class='expression'><p> <span class='randomFirst'>"+randomFirst+"</span> + <span class='randomSecond'>"+randomSecond+"</span> = ? </p></div>");
     $('.question').append(expression);
 
     //ось
-    var sprite = $("<img class='sprite' src='test_task/sprite.png'>");
+    var sprite = $("<div class='sprite'><img src='test_task/sprite.png'></div>");
     $('.question').append(sprite);
 
     //канвас для первой стрелки
     var heightAlfa = Math.round((40*randomFirst-randomFirst-1)/3);
-    var widthAlfa = (40*randomFirst-randomFirst-1);
+    var widthAlfa = (40*randomFirst-randomFirst-1)+3;
     var topAlfa = 220-heightAlfa;
-    var arrowAlfa = $("<canvas id='arrowAlfa' height="+heightAlfa+" width="+widthAlfa+"></canvas>");
+    var arrowAlfa = $("<div class='arrows'><canvas id='arrowAlfa' height="+heightAlfa+" width="+widthAlfa+"></canvas></div>");
     $('.question').append(arrowAlfa);
     $('#arrowAlfa').offset({top:topAlfa, left:45});
 
-    //отрисовка первой стрелки
-    var canvas = document.getElementById('arrowAlfa');
-    var ctx = canvas.getContext('2d');
-    ctx.beginPath();
-    ctx.moveTo(0, heightAlfa);
-    ctx.bezierCurveTo(10, 10, widthAlfa-10, 10, widthAlfa-3, heightAlfa);
-    ctx.moveTo(widthAlfa, heightAlfa-10);
-    ctx.lineTo(widthAlfa-3, heightAlfa);
-    ctx.moveTo(widthAlfa-10, heightAlfa-10);
-    ctx.lineTo(widthAlfa-3, heightAlfa);
-    ctx.stroke();
+    var drowArrow = function(height, width, arrow) {
+        var canvas = document.getElementById(arrow);
+        var ctx = canvas.getContext('2d');
+        ctx.beginPath();
+        ctx.moveTo(0, height);
+        ctx.bezierCurveTo(10, 10, width-10, 10, width-3, height);
+        ctx.moveTo(width, height-10);
+        ctx.lineTo(width-3, height);
+        ctx.moveTo(width-10, height-10);
+        ctx.lineTo(width-3, height);
+        ctx.stroke();
+    };
+
+    drowArrow(heightAlfa, widthAlfa, 'arrowAlfa');
 
     //поле ввода первого слагаемого
     var alfaInput = $("<input class='alfaInput' type='text' size='1'>");
-    $('.question').append(alfaInput);
-    $('.alfaInput').offset({top:topAlfa-10, left:widthAlfa/2});
+    $('.question').find('.arrows').append(alfaInput);
+    $('.arrows').find('.alfaInput').offset({top:topAlfa-10, left:widthAlfa/2});
 
     $('.alfaInput').on('keyup',  function() {
         var alfa = $(this).val();
@@ -43,35 +46,26 @@ $(document).ready(function() {
         if (alfa == randomFirst) {
             //если значение инпут верно, удаляем инпут, добавляем лейбел
             var alfaLab = $("<label class='alfaInput'>"+alfa+"</label>");
-            $('.alfaInput').remove();
-            $('.question').append(alfaLab);
-            $('.alfaInput').offset({top:topAlfa, left:45+widthAlfa/2});
+            $('.arrows').find('.alfaInput').remove();
+            $('.question').find('.arrows').append(alfaLab);
+            $('.arrows').find('.alfaInput').offset({top:topAlfa, left:45+widthAlfa/2});
 
             //канвас для второй стрелки
             var heightBeta = Math.round((40*randomSecond-randomSecond-1)/3);
-            var widthBeta = (40*randomSecond-randomSecond-1);
+            var widthBeta = (40*randomSecond-randomSecond-1) + 3;
             var topBeta = 220-heightBeta;
-            var leftBeta = $('#arrowAlfa').offset().left+widthAlfa;
+            var leftBeta = $('#arrowAlfa').offset().left+widthAlfa - 3;
             var arrowBeta = $("<canvas id='arrowBeta' height="+heightBeta+" width="+widthBeta+"></canvas>");
-            $('.question').append(arrowBeta);
-            $('#arrowBeta').offset({top:topBeta, left:leftBeta});
+            $('.question').find('.arrows').append(arrowBeta);
+            $('.arrows').find('#arrowBeta').offset({top:topBeta, left:leftBeta});
 
             //отрисовка второй стрелки
-            var canvas = document.getElementById('arrowBeta');
-            var ctx = canvas.getContext('2d');
-            ctx.beginPath();
-            ctx.moveTo(0, heightBeta);
-            ctx.bezierCurveTo(10, 10, widthBeta-10, 10, widthBeta-3, heightBeta);
-            ctx.moveTo(widthBeta, heightBeta-10);
-            ctx.lineTo(widthBeta-3, heightBeta);
-            ctx.moveTo(widthBeta-10, heightBeta-10);
-            ctx.lineTo(widthBeta-3, heightBeta);
-            ctx.stroke();
+            drowArrow(heightBeta, widthBeta, 'arrowBeta');
 
             //поле ввода второго слагаемого
             var betaInput = $("<input class='betaInput' type='text' size='1'>");
-            $('.question').append(betaInput);
-            $('.betaInput').offset({top:topBeta-10, left:leftBeta+widthBeta/2});
+            $('.question').find('.arrows').append(betaInput);
+            $('.arrows').find('.betaInput').offset({top:topBeta-10, left:leftBeta+widthBeta/2});
         } else {
             $(this).toggleClass("notArrow");
             $(this).closest('.question').find('.randomFirst').toggleClass("notRandom");
@@ -83,14 +77,14 @@ $(document).ready(function() {
             if (beta == randomSecond) {
                 //если значение инпут верно, удаляем инпут, добавляем лейбел
                 var betaLab = $("<label class='betaInput'>"+beta+"</label>");
-                $('.betaInput').remove();
-                $('.question').append(betaLab);
-                $('.betaInput').offset({top:topBeta, left:leftBeta+widthBeta/2});
+                $('.arrows').find('.betaInput').remove();
+                $('.question').find('.arrows').append(betaLab);
+                $('.arrows').find('.betaInput').offset({top:topBeta, left:leftBeta+widthBeta/2});
 
                 //поле ввода суммы
                 var answer = $("<input class='answer' type='text' size='1'>");
-                $('.question').append(answer);
-                $('p').text(randomFirst+' + '+randomSecond+' = ');
+                $('.question').find('.expression').append(answer);
+                $('.expression').find('p').text(randomFirst+' + '+randomSecond+' = ');
             } else {
                 $(this).toggleClass("notArrow");
                 $(this).closest('.question').find('.randomSecond').toggleClass("notRandom");
@@ -99,8 +93,8 @@ $(document).ready(function() {
             $('.answer').on('keyup', function(){
                 var an = $(this).val();
                 if (an == randomFirst+randomSecond) {
-                    $('.answer').remove();
-                    $('p').text(randomFirst+' + '+randomSecond+' = '+an);
+                    $('.expression').find('.answer').remove();
+                    $('.expression').find('p').text(randomFirst+' + '+randomSecond+' = '+an);
                 } else {
                     $(this).toggleClass("notArrow");
                 }
